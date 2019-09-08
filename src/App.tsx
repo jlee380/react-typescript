d204aa50e34cbc12e57b0b2044d19d92a5da3026
Jonghun Lee <jonghunjhl@gmail.com>
2019-09-08 14:06:25 -0400

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,47 @@ import './App.css';
 import { summarizers } from 'istanbul-lib-report';
 import { number } from 'prop-types';
 
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+interface ITodo {
+    text: string;
+    complete: boolean;
+}
+
 export default function App(): JSX.Element {
-    const [value, setValue] = useState('');
+    const [value, setValue] = useState<string>('');
+    const [todos, setTodos] = useState<ITodo[]>([]);
+
+    const handleSubmit = (e: FormElem): void => {
+        e.preventDefault();
+        addTodo(value);
+        setValue('');
+    };
+
+    const addTodo = (text: string) => {
+        const newTodos: ITodo[] = [...todos, { text, complete: false }];
+        setTodos(newTodos);
+    };
+
+    console.log(todos);
     return (
         <div className='App'>
             <Fragment>
                 <h1>Todo List</h1>
-                <form>
-                    <input type='text' required />
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type='text'
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                        required
+                    />
                     <button type='submit'>Add Todo</button>
                 </form>
+                <section>
+                    {todos.map((todo: ITodo, index: number) => {
+                        return <div key={index}>{todo.text}</div>;
+                    })}
+                </section>
             </Fragment>
         </div>
     );
